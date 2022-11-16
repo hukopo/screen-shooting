810ba7950d548b8b8fa6ed25652b9c45be6bbb1f
Anton Kholkin <32922001+hukopo@users.noreply.github.com>
2022-11-16 11:19:25 +0500

    Initial commit

---
+++ src/JobList/JobList.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function JobList() {
+  const yyyy = [1, 2, 3];
+
+  return (
+    <div className="job-list">
+      {yyyy.map((y) => (
+        <div className="job">jjj</div>
+      ))}
+    </div>
+  );
+}
---
+++ src/LayoutComponents/LeftSide.tsx
@@ -0,0 +1,5 @@
+import "./styles.css";
+
+export default function LeftSide({ children }) {
+  return <div className="left-side">{children}</div>;
+}
